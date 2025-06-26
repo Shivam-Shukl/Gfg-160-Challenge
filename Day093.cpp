3340bc1d49ed0182fa1fa81605c86e28c9d89352
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2025-06-26 18:43:13 +0530

    Create Day093.cpp
    #Day93 of #gfg160 #geekstreak2025
    
    
    
    #Problem: Lowest Common Ancestor in a BST
    
    
    
    #Approach:
    
    
    
    Utilize BST properties:
    
        If both n1 and n2 are smaller than root → LCA is in left subtree.
    
        If both are greater → LCA is in right subtree.
    
    Else, current node is the LCA.
    
    Logic:
    
     Traverse until the split point where one node is on the left and the other is on the right — this node is the lowest common ancestor.
    
    
    
    #TimeComplexity: O(h) where h = height of the tree
    
    #SpaceComplexity: O(1)
    
    Tags: #BST #LCA #TreeTraversal #BinarySearchTree #DSA

---
+++ Day093.cpp
@@ -0,0 +1,23 @@
+class Solution{
+    public:
+        Node* LCA(Node *root, int n1, int n2)
+        {
+            // code here
+            while(root != NULL)
+            {
+                if(root ->data < n1 && root -> data <n2)
+                {
+                    root = root -> right;
+                }
+                else if(root -> data >n1 && root -> data >n2)
+                {
+                    root = root -> left;
+                }
+                else
+                {
+                    return root;
+                }
+            }
+        }
+
+};
