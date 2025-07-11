f9dc1b6304d0bea011d7716fa88cb259828401a6
Shivam Shukla <116748063+Shivam-Shukl@users.noreply.github.com>
2025-07-11 13:25:00 +0530

    Create Day108.cpp

---
+++ Day108.cpp
@@ -0,0 +1,39 @@
+class Solution {
+  public:
+    vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+        // code here
+        int n = arr.size();
+        vector<int> res;
+        
+        deque<int> dq(k);
+        
+        for(int i=0;i<k;i++)
+        {
+            while(!dq.empty() && arr[i] >= arr[dq.back()])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        
+        for(int i=k;i<arr.size() ;i++)
+        {
+            res.push_back(arr[dq.front()]);
+            
+            while(!dq.empty() && dq.front() <= i-k )
+            {
+                dq.pop_front();
+            }
+            
+            while(!dq.empty() && arr[i] >= arr[dq.back()])
+            {
+                dq.pop_back();
+            }
+            dq.push_back(i);
+        }
+        
+        res.push_back(arr[dq.front()]);
+        
+        return res;
+    }
+};
